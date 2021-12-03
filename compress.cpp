75d1bcf06240b578dbf95bd5d2ff520aba752079
unglinh279 <84364281+unglinh279@users.noreply.github.com>
2021-12-03 11:50:27 +0700

    Update compress.cpp

--- compress.cpp
+++ compress.cpp
@@ -4,4 +4,4 @@ sort(values.begin(), values.end());
 values.resize(unique(values.begin(), values.end()) - values.begin());
 
 for(int i = 1; i <= n; i++)
-	arr[i] = lower_bound(values.begin(), values.end(), a[i]) - values.begin() + 1
+	arr[i] = lower_bound(values.begin(), values.end(), arr[i]) - values.begin() + 1;
