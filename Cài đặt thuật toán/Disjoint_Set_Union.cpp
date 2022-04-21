b821d5582e35bc6bbef70f1f8959c8fcdeab302b
Nha Le <63930670+nhalq@users.noreply.github.com>
2022-04-21 23:14:32 +0700

    Update Disjoint_Set_Union.cpp

--- Cài đặt thuật toán/Disjoint_Set_Union.cpp
+++ Cài đặt thuật toán/Disjoint_Set_Union.cpp
@@ -1,16 +1,38 @@
-struct DSU{
-   vector<int> par, cnt;
-   DSU(int sz) {
-      par.resize(sz); cnt.resize(sz);
-   }
-   int root(int u) { 
-      if (!par[u]) return u;
-      return par[u] = root(par[u]);
-   }
-   void connect(int u, int v) { 
-      u = root(u); v = root(v);
-      if (u == v) return;
-      if (cnt[u] < cnt[v]) swap(u, v);
-      cnt[u] += cnt[v]; par[v] = u;
-   }
+/**
+ * Disjoint-set Union
+ * Author: camnguyenmeow
+ * Date  : Dec 2, 2021
+ */
+
+struct DisjointSet {
+	vector<int> parent, counts;
+	DisjointSet(int _size) : parent(_size), counts(_size) {
+		for (int i = 0; i < _size; ++i) {
+			parent[i] = i;
+		}
+	}
+
+	// Tìm parent's node của u //
+	int p_lookup(int u) {
+		if (u == parent[u]) {
+			return u;
+		}
+
+		return parent[u] = p_lookup(parent[u]);
+	}
+
+	// Join 2 nodes lại với nhau //
+	void join(int u, int v) {
+		u = p_lookup(u);
+		v = p_lookup(v);
+
+		if (u != v) {
+			if (counts[u] < counts[v]) {
+				swap(u, v);
+			}
+
+			counts[u] += counts[v];
+			parent[v] = u;
+		}
+	}
 };
